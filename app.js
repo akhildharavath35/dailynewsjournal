a116fb6f3ba123541201397e22b150dfb8244e7a
Akhil Dharavath <akhilcruise35@gmail.com>
2020-04-27 19:56:09 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 var _ = require('lodash');
 
-mongoose.connect("mongodb://localhost:27017/journalDB", {userNewUrlParser : true}  )
+mongoose.connect("mongodb+srv://akhilcruise:akhilcruise@cluster0-7p2th.mongodb.net/journalDB", {userNewUrlParser : true}  )
 
 //post schema
 const postSchema = {
@@ -43,7 +43,7 @@ const About = new Info({
   content :"An online newspaper is the online version of a newspaper, either as a stand-alone publication or as the online version of a printed periodical.Going online created more opportunities for newspapers, such as competing with broadcast journalism in presenting breaking news in a more timely manner. The credibility and strong brand recognition of well established newspapers, and the close relationships they have with advertisers, are also seen by many in the newspaper industry as strengthening their chances of survival. The movement away from the printing process can also help decrease costs.Online newspapers, like printed newspapers, have legal restrictions regarding libel, privacy and copyright,[2] also apply to online publications in most countries as in the UK. Also, the UK Data Protection Act applies to online newspapers and news pages. Up to 2014, the PCC ruled in the UK, but there was no clear distinction between authentic online newspapers and forums or blogs. In 2007, a ruling was passed to formally regulate UK-based online newspapers, news audio, and news video websites covering the responsibilities expected of them and to clear up what is, and what isn't, an online news publication.News reporters are being taught to shoot video[5] and to write in the succinct manner necessary for internet news pages. Some newspapers have attempted to integrate the internet into every aspect of their operations, e.g., the writing of stories for both print and online, and classified advertisements appearing in both media, while other newspaper websites may be quite different from the corresponding printed newspaper."
 })
 
-//About.save()
+About.save()
 
 
 const postArray = [];
@@ -137,9 +137,12 @@ app.get('/post/:id',(req,res)=>{
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started ");
 });
